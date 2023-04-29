3ec6d151cc76110ae374f1a81e566d6d6cd47647
su55y <cesarmilano88@yahoo.com>
2023-04-29 05:14:39 +0300

    templating

---
+++ js/template.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/* exported
+  tpl
+*/
+
+const parseTpl = (tpl, obj) => {
+  const rawElm = tpl.replace(/\{\{(\w+)\}\}/g, (_, key) => obj[key] || '')
+  return new DOMParser().parseFromString(rawElm, 'text/html').body.firstChild
+}
+
+const loadTpl = (id, obj = {}) => {
+  const tpl = document.getElementById(id)?.innerHTML
+  if (tpl) return parseTpl(tpl, obj)
+}
+
+const tpl = {
+  pin: (obj) => loadTpl('pin', obj),
+  editPin: (obj) => loadTpl('edit-pin', obj),
+}
