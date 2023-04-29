3f811373c2dc3a50e97bb64aca416d002720f705
su55y <cesarmilano88@yahoo.com>
2023-04-29 22:07:50 +0300

    naming

--- js/storage.js
+++ js/storage.js
@@ -4,7 +4,7 @@
   storage
 */
 
-const PINS_STORE_KEY = 'sp_pins'
+const PINS_STORAGE_KEY = 'sp_pins'
 
 const hash = () =>
   Math.random().toString(16).slice(2).slice(-8) +
@@ -17,17 +17,17 @@ const newPin = (title, url) => ({
 })
 
 const initStorage = () => {
-  window.localStorage.getItem(PINS_STORE_KEY) ||
-    window.localStorage.setItem(PINS_STORE_KEY, '[]')
+  window.localStorage.getItem(PINS_STORAGE_KEY) ||
+    window.localStorage.setItem(PINS_STORAGE_KEY, '[]')
 }
 
 const loadPins = () => {
-  const raw_pins = window.localStorage.getItem(PINS_STORE_KEY) || '[]'
+  const raw_pins = window.localStorage.getItem(PINS_STORAGE_KEY) || '[]'
   return JSON.parse(raw_pins)
 }
 
 const updatePins = (pins) => {
-  window.localStorage.setItem(PINS_STORE_KEY, JSON.stringify(pins))
+  window.localStorage.setItem(PINS_STORAGE_KEY, JSON.stringify(pins))
 }
 
 const addPin = (title, url) => {
