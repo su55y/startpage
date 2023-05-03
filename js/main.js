41186346afeb1cb8b3970a992e4e32d5a2cfa172
su55y <cesarmilano88@yahoo.com>
2023-05-03 16:51:09 +0300

    update templates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
 /* global storage */ // storage.js
+/* global renderPins */ // pins.js
+/* global createEditForm */ // edit.js
+/* global createNewPinForm */ // new-pin.js
+/* global $ */ // dom.js
+
+const setupFormButtons = () => {
+  $.click('add', createNewPinForm)
+  $.click('edit', createEditForm)
+}
 
 window.onload = () => {
   storage.init()
+  renderPins(storage.load())
+  setupFormButtons()
 }
