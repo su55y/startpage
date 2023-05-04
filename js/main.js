40f86b80df122e46a0f29963891ca3bc6a5a03f2
su55y <cesarmilano88@yahoo.com>
2023-05-04 18:18:01 +0300

    handle escape to close forms

--- js/main.js
+++ js/main.js
@@ -3,14 +3,22 @@
 /* global createEditForm */ // edit.js
 /* global createNewPinForm */ // new-pin.js
 /* global $ */ // dom.js
+/* global consts */ // consts.js
 
 const setupFormButtons = () => {
   $.click('add', createNewPinForm)
   $.click('edit', createEditForm)
 }
 
+const handleEscape = (e) => {
+  if (e?.key !== 'Escape') return
+  $.get(consts.new_pin_block_id)?.remove()
+  $.get(consts.edit_pins_block_id)?.remove()
+}
+
 window.onload = () => {
   storage.init()
   renderPins(storage.load())
   setupFormButtons()
+  document.body.addEventListener('keyup', handleEscape)
 }
