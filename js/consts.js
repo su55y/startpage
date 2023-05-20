4a9e767fc034623f66a584e8bb03f9cb5d424090
su55y <cesarmilano88@yahoo.com>
2023-05-20 06:20:15 +0300

    simplify consts

--- js/consts.js
+++ js/consts.js
@@ -3,28 +3,17 @@
   consts
 */
 
-const NEW_PIN_BLOCK_ID = 'new-pin-block',
-  NEW_PIN_CANCEL_ID = 'new-pin-cancel',
-  NEW_PIN_SAVE_ID = 'new_pin_save_id',
-  NEW_TITLE_ID = 'new-title',
-  NEW_URL_ID = 'new-url',
-  NEW_ICON_ID = 'new-icon',
-  NEW_ICON_URL_ID = 'new-icon-url',
-  EDIT_PINS_BLOCK_ID = 'edit-pins-block',
-  EDIT_PINS_LIST_ID = 'edit-pins-list',
-  EDIT_PINS_CANCEL_ID = 'edit-pins-cancel'
-
 const consts = {
   /* new pin form */
-  new_pin_block_id: NEW_PIN_BLOCK_ID,
-  new_pin_cancel_id: NEW_PIN_CANCEL_ID,
-  new_pin_save_id: NEW_PIN_SAVE_ID,
-  new_title_id: NEW_TITLE_ID,
-  new_url_id: NEW_URL_ID,
-  new_icon_id: NEW_ICON_ID,
-  new_icon_url_id: NEW_ICON_URL_ID,
+  new_pin_block_id: 'new-pin-block',
+  new_pin_cancel_id: 'new-pin-cancel',
+  new_pin_save_id: 'new-pin-save',
+  new_title_id: 'new-title',
+  new_url_id: 'new-url',
+  new_icon_id: 'new-icon',
+  new_icon_url_id: 'new-icon-url',
   /* edit pins form */
-  edit_pins_block_id: EDIT_PINS_BLOCK_ID,
-  edit_pins_list_id: EDIT_PINS_LIST_ID,
-  edit_pins_cancel_id: EDIT_PINS_CANCEL_ID,
+  edit_pins_block_id: 'edit-pins-block',
+  edit_pins_list_id: 'edit-pins-list',
+  edit_pins_cancel_id: 'edit-pins-cancel',
 }
